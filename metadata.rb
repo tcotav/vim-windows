e5f2c39a2be210cf66b9dca0feaa7ecf8c4b1053
James Francis <tcotav@gmail.com>
2013-05-29 17:52:29 -0700

    bumping version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tcotav@gmail.com'
 license          'MIT'
 description      'Installs/Configures vim-windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends "windows"
 depends "powershell"
