e2892d69315b11dc9025735835c4e9ce99df538c
James Francis <tcotav@gmail.com>
2013-06-14 14:58:59 -0700

    bumped version number for _vimrc add

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tcotav@gmail.com'
 license          'MIT'
 description      'Installs/Configures vim-windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 depends "windows"
 depends "powershell"
